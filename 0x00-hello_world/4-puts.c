d0b68fa4d7c87a4ccb348c39d3be770db54a9a54
Amira_Elsayed <e.amira67@gmail.com>
2022-10-13 13:35:12 -0700

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,35 +1,15 @@
 /*
- *
- *  * File: 4-puts.c
- *
- *
- *    
- */
-
-
-#include <stdio.h>
-
+* here we go
+*/
+#include<stdio.h>
 /*
- *
- *
- *  * main - Entry point
- *
- *  
- *
- *  * Return: Always 0
- *  
- *   
- */
-
+*main entery point
+*
+*
+* return always suc 0
+*/
 int main(void)
-
 {
-	/*
-	 * accept pleeeessss
-	 *
-	*/
-puts("\"Programming is like building a multilingual puzzle");
-
-return (0);
-
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
