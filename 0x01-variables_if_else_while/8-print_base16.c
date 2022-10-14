b53857d6cdadf925202e6a07e17c9064f0584d12
Amira_Elsayed <e.amira67@gmail.com>
2022-10-14 15:24:30 -0700

    8. Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+/*
+*file : 0-positive_or_negative.c
+*Author:Amira
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+*main - prints the type
+*of the number
+*
+*Return:  Always 0.
+*
+*/
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int a = 48;
+
+	for (; a <= 57; a++)
+		putchar((char)a);
+	for (a = 97; a <= 'f'; a++)
+		putchar((char)a);
+	putchar('\n');
+
+	return (0);
+
+}
