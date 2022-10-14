f3a6e0c132facffec775e36ff6cb38d7ca8e9050
Amira_Elsayed <e.amira67@gmail.com>
2022-10-14 14:55:45 -0700

    5. Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,26 @@
+/*
+*file : 0-positive_or_negative.c
+*Author:Amira
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+*main - prints the type
+*of the number
+*
+*Return:  Always 0.
+*
+*/
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int a = 0;
+
+	for (; a <= 9; a++)
+		printf(a);
+	putchar('\n');
+
+	return (0);
+
+}
