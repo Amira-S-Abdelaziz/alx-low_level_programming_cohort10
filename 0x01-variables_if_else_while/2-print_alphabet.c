c40b21ba49ffd11ddc3ab751f4b4562001453334
Amira_Elsayed <e.amira67@gmail.com>
2022-10-14 14:35:33 -0700

    2. I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,26 @@
+/*
+*file : 0-positive_or_negative.c
+*Author:Amira
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+*main - prints the type
+*of the number
+*
+*Return:  Always 0.
+*
+*/
+/* betty style doc for function main goes there */
+int main(void)
+{
+	char a = 'a';
+
+	for (; a <= 'z'; a++)
+		putchar(a);
+	putchar('\n');
+
+	return (0);
+
+}
