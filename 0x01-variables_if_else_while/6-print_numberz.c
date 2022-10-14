c6165381ddb720daa11bd724a3cde379124a2f0e
Amira_Elsayed <e.amira67@gmail.com>
2022-10-14 15:03:32 -0700

    6. Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,26 @@
+/*
+*file : 0-positive_or_negative.c
+*Author:Amira
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+*main - prints the type
+*of the number
+*
+*Return:  Always 0.
+*
+*/
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int a = 48;
+
+	for (; a <= 57; a++)
+		putchar((char)a);
+	putchar('\n');
+
+	return (0);
+
+}
