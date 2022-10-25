19bafe3f40e4bbd6b735754f71d1d94536d7311c
Amira_Elsayed <e.amira67@gmail.com>
2022-10-25 05:25:30 -0700

    9. strcpy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -8,21 +8,24 @@
 char *_strcpy(char *dest, char *src)
 {
 	int len = 0;
+	int len2 = 0;
 	int i = 0;
-	char s[1000000];
 
 	while (*(src + len) != '\0')
 	{
 		len++;
 	}
-	for (; i < len; i++)
+	while (*(dest + len2) != '\0')
 	{
-		*(s + i) = *(src + i);
+		len2++;
 	}
-	i = 0;
 	for (; i < len; i++)
 	{
-		*(dest + i) = s[i];
+		*(dest + i) = *(src + i);
+	}
+	for (; i < len2; i++)
+	{
+		*(dest + i) = '\0';
 	}
 	return (src);
 }
