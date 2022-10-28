627a21cfe80dd4d63285490fc1725f01c76e5cc3
Amira_Elsayed <e.amira67@gmail.com>
2022-10-28 05:07:18 -0700

    3. strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,4 +7,15 @@
 */
 int _strcmp(char *s1, char *s2)
 {
+	int ans=0;
+	int i = 0;
 
+	while (ans == 0)
+	{
+		if (*(s1 + i) == '\0' && *(s2 + i) == '\0')
+			break;
+		ans = *(s1 + i) - *(s2 + i);
+		i++;
+	}	
+	return (ans);
+}
