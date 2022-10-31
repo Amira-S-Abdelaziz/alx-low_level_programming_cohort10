b2fbf8d0d15fabb4ced2013b407a5987779dd342
Amira_Elsayed <e.amira67@gmail.com>
2022-10-31 16:42:25 -0700

    0. memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -9,10 +9,21 @@
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i = 0;
+	int flag = 0;
 
 	for (; i < n; i++)
 	{
+		if (*(s + i) == '\0')
+			flag = 1;
 		*(s + i) = b;
+	}
+	if (flag == 0)
+	{
+		while (*(s + i) != '\0')
+		{
+			*(s + i) = '\0';
+			i++;
+		}
 	}
 	*(s + i) = '\0';
 	return (s);
