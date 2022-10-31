354064229a2fb17765e68f5edb1f39bf107b9504
Amira_Elsayed <e.amira67@gmail.com>
2022-10-31 16:36:38 -0700

    0. memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+*_memset - function
+*@s: var
+*@b: char
+*@n: integer
+*Return: value
+*/
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i = 0;
+
+	for (; i < n; i++)
+	{
+		*(s + i) = b;
+	}
+	*(s + i) = '\0';
+	return (s);
+}
