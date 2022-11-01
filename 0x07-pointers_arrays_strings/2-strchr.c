d3a99fc71efcec8d431dc2a2466c1ffbd90c2a08
Amira_Elsayed <e.amira67@gmail.com>
2022-10-31 17:19:01 -0700

    2. strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -8,6 +8,7 @@
 char *_strchr(char *s, char c)
 {
 	int i = 0;
+	char *p = NULL;
 
 	while (*(s + i) != '\0')
 	{
@@ -17,5 +18,5 @@ char *_strchr(char *s, char c)
 		}
 			i++;
 	}
-	return (NULL);
+	return (p);
 }
