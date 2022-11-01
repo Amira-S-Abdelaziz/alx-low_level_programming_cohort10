e604507139f695f0c475c1898ceebbb18b61ecce
Amira_Elsayed <e.amira67@gmail.com>
2022-11-01 03:13:20 -0700

    3. strspn
    mandatory

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -10,13 +10,17 @@ unsigned int _strspn(char *s, char *accept)
 	int mx = 0;
 	int i = 0;
 	int j = 0;
+	int flag = 0;
 
 	for (; *(accept + i) != '\0'; i++)
 	{
+		flag = 0;
 		for (j = 0; *(s + j) != '\0'; j++)
 		{
+
 			if (*(accept + i) == *(s + j))
 			{
+				flag = 1;
 				if (mx < (j + 1))
 				{
 					mx = j + 1;
@@ -24,6 +28,8 @@ unsigned int _strspn(char *s, char *accept)
 				break;
 			}
 		}
+		if (flag == 0)
+			break;
 	}
 	return (mx);
 }
