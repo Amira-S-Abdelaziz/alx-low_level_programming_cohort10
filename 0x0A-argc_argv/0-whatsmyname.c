9e71febaa5cb2f8cebdd5e18b85dd9b8861c1162
Amira_Elsayed <e.amira67@gmail.com>
2022-11-04 04:40:07 -0700

    0. It ain't what they call you, it's what you answer to

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+*main - function
+*@argc: counter
+*@argv: value
+*Return: value
+*/
+int main(int agrc, char *agrv[])
+{
+	if (agrc > 0)
+		printf("%s\n", agrv[0]);
+	return (0);
+}
