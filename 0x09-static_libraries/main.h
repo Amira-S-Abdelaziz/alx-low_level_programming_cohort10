d6979768b33818b6539826a244b5e1769c1af298
MidoriyaArima3451 <113601076+MidoriyaArima3451@users.noreply.github.com>
2022-11-07 23:26:28 +0200

    Update main.h

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,5 +1,5 @@
-#ifndef
-#define 
+#ifndef _SAMPLE_HEADER_H_
+#define _SAMPLE_HEADER_H_
 
 int _putchar(char c);
 int _islower(int c);
@@ -22,4 +22,4 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
-#endif
+#endif /* _SAMPLE_HEADER_H_ */
