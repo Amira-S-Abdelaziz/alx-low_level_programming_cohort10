ed821f751a9d80695bb6d69a765ebdde1d5c7640
Amira_Elsayed <e.amira67@gmail.com>
2022-11-12 15:19:39 -0800

    2. He who is not courageous enough to take risks will accomplish nothing in life

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,10 +11,12 @@ char *str_concat(char *s1, char *s2)
 	char *s;
 	unsigned int i = 0, sz1 = 0, sz2 = 0, sz = 0;
 
+	if (s1 != NULL)
 	while (*(s1 + sz1) != '\0')
 	{
 		sz1++;
 	}
+	if (s2 != NULL)
 	while (*(s2 + sz2) != '\0')
 	{
 		sz2++;
