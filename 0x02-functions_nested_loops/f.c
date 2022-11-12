e0c2f9d6cc5ce1f1592a10336ba2bc4d77bb8789
Amira_Elsayed <e.amira67@gmail.com>
2022-11-12 12:41:55 -0800

    rm

---
+++ 0x02-functions_nested_loops/f.c
@@ -0,0 +1,21 @@
+/*
+*file : 0-positive_or_negative.c
+*Author:Amira
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "main.h"
+/**
+*add - prints the type
+*of the number
+*@a: an integer char
+*@b: an integer
+*Return:  Always 0.
+*
+*/
+/* betty style doc for function main goes there */
+int add(int a, int b)
+{
+		return (a + b);
+}
