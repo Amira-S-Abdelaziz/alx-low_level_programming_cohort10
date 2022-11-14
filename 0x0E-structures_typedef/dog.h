a5c91482ddab614529ed20a73e1d502d6cac7bff
Amira_Elsayed <e.amira67@gmail.com>
2022-11-14 13:47:40 -0800

    0. Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef _MAIN_
+#define _MAIN_
+
+/**
+* struct dog - structure for dogs
+*@name: var
+*@age : var
+*@owner: var
+*
+*Description: a struct
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
