d04e7efc7b8f4e5ab680e324bd6cda8072ad9a56
Amira_Elsayed <e.amira67@gmail.com>
2022-11-18 13:52:04 -0800

    unfinished

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "3-calc.h"
+/**
+*main - function
+*@agrc: var
+*@agrv: var
+*Return: 0
+*/
+int main(int agrc, char *agrv[])
+{
+	int num1,num2;
+	int (*o)(int, int);
+
+	if (agrc != 4)
+	{
+		printf("Error\n");
+		return (98);
+	}
+	if (agrv[2][1])
+	{
+		printf("Error\n");
+		return (99);
+	}
+
+	num1 = atoi(agrv[1]);
+	num2 = atoi(agrv[3]);
+	o = get_op_func(agrv[2]);
+	if (o == NULL || agrv[2][1])
+	{
+		printf("Error\n");
+		return (99);
+	}
+	printf("%d\n",o(num1,num2));
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include "3-calc.h"
+/**
+*op_add - function
+*@a: var
+*@b: var
+*Return: value
+*/
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+*op_sub - function
+*@a: var
+*@b: var
+*Return: value
+*/
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+*op_mul - function
+*@a: var
+*@b: var
+*Return: value
+*/
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+*op_div - function
+*@a: var
+*@b: var
+*Return: value
+*/
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+*op_mod - fucntion
+*@a: var
+*@b: var
+*Return: value
+*/
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
