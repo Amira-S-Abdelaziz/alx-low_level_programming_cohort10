8d820c90a10b5f09abd2e862c4e65b9ed52a0e06
Amira_Elsayed <e.amira67@gmail.com>
2022-11-24 15:44:55 -0800

    4. Free list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+*free_list - function
+*@head: var
+*/
+void free_list(list_t *head)
+{
+	list_t *cur;
+	while (head != NULL)
+	{
+		cur = head;
+		head = head->next;
+		free(cur);
+	}
+}
