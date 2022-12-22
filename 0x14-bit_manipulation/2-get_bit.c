e053527859c3cf0f5481289c3c23c7c0bb720e9e
Amira_Elsayed <e.amira67@gmail.com>
2022-12-22 14:46:55 -0800

    10

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -9,17 +9,18 @@
 int get_bit(unsigned long int n, unsigned int index)
 {
 	int a = 0;
+	unsigned int i = 0;
 
 	if (index >= 64)
 		return (-1);
-	while (index != 0)
+	while (index != i)
 	{
 		n = n >> 1;
 		if ((n & 1) == 1)
 			a = 1;
 		else
 			a = 0;
-		index--;
+		i++;
 	}
 	return (a);
 }
